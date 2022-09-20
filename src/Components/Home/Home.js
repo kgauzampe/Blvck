8cf7251fa38a4ae398d9da1c72ce46f0f855682b
kgauzampe <kgauza.mpe@gmail.com>
2022-09-20 10:40:56 +0200

    files added

---
+++ src/Components/Home/Home.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Container,Typography, Button } from '@material-ui/core';
+
+
+
+const Home = () => {
+    return(
+        <div className="home-Container">
+            <Container>
+            <Typography variant="h3">Welcome</Typography>
+
+            </Container>
+        </div>
+        
+    )
+}
+export default Home
