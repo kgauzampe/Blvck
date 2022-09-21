f9156a6e301bf7408909f772804753b2f0226857
kgauzampe <kgauza.mpe@gmail.com>
2022-09-21 19:26:33 +0200

    Page content added

--- src/Components/Store/Store.js
+++ src/Components/Store/Store.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import { Container,Typography, Button, Grid } from '@material-ui/core';
+import Navbar from "../NavBar/Navbar";
 
 
 
 const Store = () => {
 
     return (
-        <>
-        
-        </>
+        <div className='Store-head'>
+
+        </div>
     )
 }
