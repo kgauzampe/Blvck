c1f0833404f988cc73050a2c2f168ae177188971
kgauzampe <kgauza.mpe@gmail.com>
2022-10-11 10:38:14 +0200

    store Update

--- src/Components/Store/Store.js
+++ src/Components/Store/Store.js
@@ -1,18 +1,60 @@
 import React from 'react';
 import { Container,Typography, Button, Grid } from '@material-ui/core';
 import Navbar from "../NavBar/Navbar";
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
 
 
-
+const useStyles = makeStyles({
+    root: {
+      maxWidth: 345,
+    },
+    media: {
+      height: 140,
+    },
+  });
 const Store = () => {
-
+    const classes = useStyles();
     return (
         <div className='Store-head'>
             <Navbar/>
             <div className='Store-con'>
                 <Typography>Welcome to the Market</Typography>
+                    <Card className={classes.root}>
+                        <CardActionArea>
+                            <CardMedia
+                            className={classes.media}
+                            image="/static/images/cards/contemplative-reptile.jpg"
+                            title="Contemplative Reptile"
+                            />
+                            <CardContent>
+                            <Typography gutterBottom variant="h5" component="h2">
+                                Lizard
+                            </Typography>
+                            <Typography variant="body2" color="textSecondary" component="p">
+                                Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+                                across all continents except Antarctica
+                            </Typography>
+                            </CardContent>
+                        </CardActionArea>
+                        <CardActions>
+                            <Button size="small" color="primary">
+                            Share
+                            </Button>
+                            <Button size="small" color="primary">
+                            Learn More
+                            </Button>
+                        </CardActions>
+                    </Card>
             </div>
 
         </div>
     )
 }
+
+
+
