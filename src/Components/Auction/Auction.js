54461e80f220f7a03529e7f3b4b313934eec5473
kgauzampe <kgauza.mpe@gmail.com>
2022-10-12 18:56:34 +0200

    Auction update

--- src/Components/Auction/Auction.js
+++ src/Components/Auction/Auction.js
@@ -27,32 +27,7 @@ const Auction = () => {
             <Container>
                 <div className='cards'>
                     <Typography variant='h3' className='AuctTypo'>Place your Bid</Typography>
-                    <Card className={classes.root}>
-                        <CardActionArea>
-                            <CardMedia
-                            className={classes.media}
-                            image="/static/images/cards/contemplative-reptile.jpg"
-                            title="Contemplative Reptile"
-                            />
-                            <CardContent>
-                            <Typography gutterBottom variant="h5" component="h2">
-                                Lizard
-                            </Typography>
-                            <Typography variant="body2" color="textSecondary" component="p">
-                                Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
-                                across all continents except Antarctica
-                            </Typography>
-                            </CardContent>
-                        </CardActionArea>
-                        <CardActions>
-                            <Button size="small" color="primary">
-                            Share
-                            </Button>
-                            <Button size="small" color="primary">
-                            Learn More
-                            </Button>
-                        </CardActions>
-                    </Card>
+                   
                 </div>
             </Container>
         </div>
